1939d1ec134243df791029d41575744d388dc1f4
zwkno1 <zwkno1@gmail.com>
2015-07-21 20:53:46 +0800

    增加了ffmpeg枚举系统设备

--- server/server/include/avdevicefactory.hpp
+++ server/server/include/avdevicefactory.hpp
@@ -7,6 +7,13 @@ class AVDeviceFactory
 public:
 	AVDeviceFactory();
 	~AVDeviceFactory();
+	int is_device(const AVClass *avclass)
+	{
+		if (!avclass)
+			return 0;
+			return AV_IS_INPUT_DEVICE(avclass->category) || AV_IS_OUTPUT_DEVICE(avclass->category);
+	}
+	int show_devices();
 };
 
 AV_NAMESPACE_END
--- server/server/src/avdevicefactory.cpp
+++ server/server/src/avdevicefactory.cpp
@@ -12,4 +12,69 @@ AVDeviceFactory::~AVDeviceFactory()
 {
 }
 
+int AVDeviceFactory::show_devices()
+{
+	AVInputFormat *ifmt  = NULL;
+	AVOutputFormat *ofmt = NULL;
+	const char *last_name;
+	int is_dev;
+
+	printf("%s\n"
+			" D. = Demuxing supported\n"
+			" .E = Muxing supported\n"
+			" --\n", "Devices:");
+	last_name = "000";
+	for (;;) {
+		int decode = 0;
+		int encode = 0;
+		const char *name      = NULL;
+		const char *long_name = NULL;
+
+		while ((ofmt = av_oformat_next(ofmt))) {
+			if(is_device(ofmt->priv_class)){
+				if ((!name || strcmp(ofmt->name, name) < 0) &&
+						strcmp(ofmt->name, last_name) > 0) {
+					name      = ofmt->name;
+					long_name = ofmt->long_name;
+					encode    = 1;
+				}
+			}
+		}
+		while ((ifmt = av_iformat_next(ifmt))) {
+			if(is_device(ifmt->priv_class)){
+				if ((!name || strcmp(ifmt->name, name) < 0) &&
+						strcmp(ifmt->name, last_name) > 0) {
+					name      = ifmt->name;
+					long_name = ifmt->long_name;
+					encode    = 0;
+				}
+				if (name && strcmp(ifmt->name, name) == 0)
+					decode = 1;
+			}
+		}
+		if (!name)
+			break;
+		last_name = name;
+
+		printf(" %s%s %-15s %s\n",
+				decode ? "D" : " ",
+				encode ? "E" : " ",
+				name,
+				long_name ? long_name:" ");
+	}
+	return 0;
+}
+
 AV_NAMESPACE_END
+
+#if 0
+int main()
+{
+	av_register_all();
+	avformat_network_init();
+	avdevice_register_all();
+	av::AVDeviceFactory factory;
+	factory.show_devices();
+	return 0;
+}
+#endif
